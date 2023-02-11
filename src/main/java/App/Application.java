288a09c09ee7fed84aa61f4cc1cc6d441bc335fc
SCH239NET\antonovmo.24 <maksimgandonov@makima.shluxa>
2023-02-11 10:20:19 +0300

    try3

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -1,4 +1,12 @@
 package App;
 
-public class Application {
+import io.github.humbleui.jwm.Event;
+
+import java.util.function.Consumer;
+
+public class Application implements Consumer<Event> {
+    @Override
+    public void accept(Event event) {
+
+    }
 }
