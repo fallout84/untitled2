07f233ba26d6eb8b226a3c33c228dfca3899794a
fallout84 <mathsmaxim@gmail.com>
2023-02-18 22:03:10 +0300

    add setColor()

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -1,6 +1,8 @@
 package App;
 
 import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Surface;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -17,13 +19,34 @@ public class Application implements Consumer<Event> {
         window = App.makeWindow();
         window.setEventListener(this);
         window.setTitle("Java 2D");
-        window.setWindowSize(900, 900);
+        window.setWindowSize(1800, 900);
         window.setWindowPosition(100, 100);
+
         // задаём иконку
         switch (Platform.CURRENT) {
             case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
             case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
         }
+        // названия слоёв, которые будем перебирать
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+
+        // перебираем слои
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        // если окну не присвоен ни один из слоёв
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
         window.setVisible(true);
         /**
          *
@@ -43,7 +66,12 @@ public class Application implements Consumer<Event> {
             // завершаем работу приложения
             App.terminate();
         } else if (e instanceof EventWindowCloseRequest) {
-            window.close();
+            window.close();}
+                    else if (e instanceof EventFrameSkija ee) {
+                // получаем поверхность рисования
+                Surface s = ee.getSurface();
+                // очищаем её канвас заданным цветом
+            s.getCanvas().clear(Colors.APP_BACKGROUND_COLOR);
+            }
         }
     }
-}
